ac6f5c95573488592c9c5cacbbd05fb9ca0057c8
Lunya <ludovic.sannicolas@gmail.com>
2017-12-07 23:23:39 +0100

    Mise en place de la navbar, ais sans la couche metier.

--- src/front-src/src/app/app.module.ts
+++ src/front-src/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FootbarComponent } from './footbar/footbar.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HomeComponent
+		HomeComponent,
+		NavbarComponent,
+		FootbarComponent
 	],
 	imports: [
 		BrowserModule,
+		NgbModule.forRoot(),
 		AppRoutingModule
 	],
 	providers: [],
