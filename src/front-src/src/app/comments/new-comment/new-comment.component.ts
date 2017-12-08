a910f9409de436a02595b36c70aa580cf7855ad4
Lunya <ludovic.sannicolas@gmail.com>
2017-12-08 05:55:35 +0100

    Formulaire insertion OK

--- src/front-src/src/app/comments/new-comment/new-comment.component.ts
+++ src/front-src/src/app/comments/new-comment/new-comment.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
+import { HttpClient } from '@angular/common/http';
+import { PlatformLocation } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-new-comment',
@@ -11,18 +14,27 @@ export class NewCommentComponent implements OnInit {
 	private commentForm: FormGroup;
 
 	constructor(
-		private fb: FormBuilder
+		private fb: FormBuilder,
+		private http: HttpClient,
+		private platformLocation: PlatformLocation,
+		private router: Router
 	) {
 		this.commentForm = this.fb.group({
-			pseudo: [null, [Validators.required, CustomValidators.gte(3)]],
+			pseudo: [null, [Validators.required, Validators.minLength(3)]],
 			email: [null, [Validators.required, CustomValidators.email]],
-			title: [null, [Validators.required, CustomValidators.gte(6)]],
-			comment: [null, [Validators.required, CustomValidators.gte(30)]]
+			title: [null, [Validators.required, Validators.minLength(6)]],
+			comment: [null, [Validators.required, Validators.minLength(10)]]
 		});
 	}
 
 	ngOnInit() {
 	}
 
-	onSubmitComment() {}
+	onSubmitComment() {
+		this.http.post(this.platformLocation.location.origin + '/api/comment', this.commentForm.value)
+			.subscribe(result => {
+				this.router.navigate(['/']).catch(err => console.log(err));
+			}
+		);
+	}
 }
