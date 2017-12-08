30d93ecd40252b471ce8c7b3fbdb29250f093b55
Margaux Seoane <naoru@Exagames.home>
2017-12-08 02:43:17 +0100

    website

--- src/front-src/src/app/app-routing.module.ts
+++ src/front-src/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { EmergencyContactComponent } from './emergency-contact/emergency-contact.component';
+import { GoodPracticesComponent } from './good-practices/good-practices.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: '#', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
-	{ path: 'emergency', component: EmergencyContactComponent }
+	{ path: 'emergency', component: EmergencyContactComponent },
+	{ path: 'practices', component: GoodPracticesComponent }
 ];
 
 
